56448dd24d578a4f3cf17ac20d73b13822f9e200
arisohib <danielsohib@gmail.com>
2024-05-02 14:32:15 +0700

    update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -37,15 +37,15 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-text mx-3">PLN UP2D Jawa Timur</div>
+                <div class="sidebar-brand-text mx-3">PENS</div>
             </a>
 
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item {{ Nav::isRoute('dashboard') }}">
-                <a class="nav-link" href="{{ route('dashboard') }}">
+            <li class="nav-item">
+                <a class="nav-link" href="">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>{{ __('Dashboard') }}</span></a>
             </li>
@@ -58,97 +58,42 @@
                 {{ __('Settings') }}
             </div>
 
-            <li class="nav-item {{ Nav::isRoute('smartsoil1.index') }}">
-                <a class="nav-link" href="{{ route('smartsoil1.index') }}">
-                    <i class="fas fa-fw fa-ruler-combined"></i>
-                    <span>{{ __('Smart Soil 1') }}</span>
+            <li class="nav-item {{ Nav::isRoute('indexHome') }}">
+                <a class="nav-link" href="{{ route('indexHome') }}">
+                    <i class="fas fa-fw fa-kiwi-bird"></i>
+                    <span>{{ __('Smart Agriculture') }}</span>
                 </a>
             </li>
 
-            <li class="nav-item {{ Nav::isRoute('smartsoil2.index') }}">
-                <a class="nav-link" href="{{ route('smartsoil2.index') }}">
-                    <i class="fas fa-fw fa-ruler-combined"></i>
-                    <span>{{ __('Smart Soil 2') }}</span>
+            <li class="nav-item ">
+                <a class="nav-link" href="">
+                    <i class="fas fa-fw fa-kiwi-bird"></i>
+                    <span>{{ __('Smart Soil') }}</span>
                 </a>
             </li>
 
-
-
-            {{-- <!-- Nav Item - Profile -->
-            <li class="nav-item {{ Nav::isRoute('pengukuran') }}">
-                <a class="nav-link" href="{{ route('pengukuran') }}">
-                    <i class="fas fa-fw fa-ruler-combined"></i>
-                    <span>{{ __('Pengukuran') }}</span>
+            <li class="nav-item ">
+                <a class="nav-link" href="">
+                    <i class="fas fa-fw fa-bucket"></i>
+                    <span>{{ __('Smart Irigation') }}</span>
                 </a>
             </li>
 
-            <li class="nav-item {{ Nav::isRoute('kinerja') }}">
-                <a class="nav-link" href="{{ route('kinerja') }}">
-                    <i class="fas fa-fw fa-industry"></i>
-                    <span>{{ __('Kinerja') }}</span>
+            <li class="nav-item ">
+                <a class="nav-link" href="">
+                    <i class="fas fa-fw fa-cloud-rain"></i>
+                    <span>{{ __('Smart Weather') }}</span>
                 </a>
             </li>
 
-            <li class="nav-item {{ Nav::isRoute('WOIndex') }}">
-                <a class="nav-link" href="{{ route('WOIndex') }}">
-                    <i class="fas fa-fw fa-pie-chart"></i>
-                    <span>{{ __('Work Order') }}</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-toolbox"></i>
-                    <span>Asset</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Menu Asset:</h6>
-                        <a class="collapse-item" href="{{ route('AssetMenu') }}">
-                            Asset Keypoint
-                        </a>
-                        <a class="collapse-item" href="{{ route('rtu') }}">
-                            Asset RTU
-                        </a>
-                        <a class="collapse-item" href="{{ route('rectifier') }}">
-                            Asset Rectifier
-                        </a>
-
-                        <a class="collapse-item" href="{{ route('battery') }}">
-                            Asset Battere
-                        </a>
-
-                        <a class="collapse-item" href="{{ route('server') }}">
-                            Asset Server
-                        </a>
-                        <a class="collapse-item" href="{{ route('gateway') }}">
-                            Asset Gateway
-                        </a>
-
-                        <a class="collapse-item" href="{{ route('radio') }}">
-                            Radio
-                        </a>
-                        <a class="collapse-item" href="{{ route('tagging') }}">
-                            Data Tagging
-                        </a>
-                    </div>
-                </div>
-            </li>
-
-            <li class="nav-item {{ Nav::isRoute('Komisioning') }}">
-                <a class="nav-link" href="{{ route('Komisioning') }}">
-                    <i class="fas fa-fw fa-dollar-sign"></i>
-                    <span>{{ __('Komisioning') }}</span>
+            <li class="nav-item ">
+                <a class="nav-link" href="">
+                    <i class="fas fa-fw fa-torii-gate"></i>
+                    <span>{{ __('Smart Monitoring HPT') }}</span>
                 </a>
             </li>
 
-            <!-- Nav Item - Profile -->
-            <li class="nav-item {{ Nav::isRoute('profile') }}">
-                <a class="nav-link" href="{{ route('profile') }}">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span>{{ __('Profile') }}</span>
-                </a>
-            </li> --}}
+            
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
 
@@ -215,126 +160,6 @@
                             </div>
                         </li>
 
-                        <!-- Nav Item - Alerts -->
-                        <li class="nav-item dropdown no-arrow mx-1">
-                            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-bell fa-fw"></i>
-                                <!-- Counter - Alerts -->
-                                <span class="badge badge-danger badge-counter">3+</span>
-                            </a>
-                            <!-- Dropdown - Alerts -->
-                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="alertsDropdown">
-                                <h6 class="dropdown-header">
-                                    Alerts Center
-                                </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-primary">
-                                            <i class="fas fa-file-alt text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 12, 2019</div>
-                                        <span class="font-weight-bold">A new monthly report is ready to
-                                            download!</span>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-success">
-                                            <i class="fas fa-donate text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 7, 2019</div>
-                                        $290.29 has been deposited into your account!
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-warning">
-                                            <i class="fas fa-exclamation-triangle text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 2, 2019</div>
-                                        Spending Alert: We've noticed unusually high spending for your account.
-                                    </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Show All
-                                    Alerts</a>
-                            </div>
-                        </li>
-
-                        <!-- Nav Item - Messages -->
-                        <li class="nav-item dropdown no-arrow mx-1">
-                            <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-envelope fa-fw"></i>
-                                <!-- Counter - Messages -->
-                                <span class="badge badge-danger badge-counter">7</span>
-                            </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="messagesDropdown">
-                                <h6 class="dropdown-header">
-                                    Message Center
-                                </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle"
-                                            src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt="">
-                                        <div class="status-indicator bg-success"></div>
-                                    </div>
-                                    <div class="font-weight-bold">
-                                        <div class="text-truncate">Hi there! I am wondering if you can help me with a
-                                            problem I've been having.</div>
-                                        <div class="small text-gray-500">Emily Fowler · 58m</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle"
-                                            src="https://source.unsplash.com/AU4VPcFN4LE/60x60" alt="">
-                                        <div class="status-indicator"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">I have the photos that you ordered last month, how
-                                            would you like them sent to you?</div>
-                                        <div class="small text-gray-500">Jae Chun · 1d</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle"
-                                            src="https://source.unsplash.com/CS2uCrpNzJY/60x60" alt="">
-                                        <div class="status-indicator bg-warning"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">Last month's report looks great, I am very happy
-                                            with the progress so far, keep up the good work!</div>
-                                        <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle"
-                                            src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="">
-                                        <div class="status-indicator bg-success"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">Am I a good boy? The reason I ask is because someone
-                                            told me that people say this to all dogs, even if they aren't good...</div>
-                                        <div class="small text-gray-500">Chicken the Dog · 2w</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Read More
-                                    Messages</a>
-                            </div>
-                        </li>
-
                         <div class="topbar-divider d-none d-sm-block"></div>
 
                         <!-- Nav Item - User Information -->
@@ -345,27 +170,6 @@
                                     class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
                                 <img class="img-profile rounded-circle" src="{{ asset('Image/undraw_profile.svg') }}"> </a>
                             <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="{{ route('profile') }}">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    {{ __('Profile') }}
-                                </a>
-                                <a class="dropdown-item" href="javascript:void(0)">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    {{ __('Settings') }}
-                                </a>
-                                <a class="dropdown-item" href="javascript:void(0)">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    {{ __('Activity Log') }}
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal"
-                                    data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    {{ __('Logout') }}
-                                </a>
-                            </div>
                         </li>
 
                     </ul>
