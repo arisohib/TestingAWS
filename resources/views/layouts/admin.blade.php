3f030c8b95077b988dcc8722d75c881bada28f4f
arisohib <danielsohib@gmail.com>
2024-05-25 21:44:37 +0700

    update description

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Laravel SB Admin 2">
+    <meta name="description" content="TaniPintar Platform">
     <meta name="author" content="Alejandro RH">
 
     <!-- CSRF Token -->
